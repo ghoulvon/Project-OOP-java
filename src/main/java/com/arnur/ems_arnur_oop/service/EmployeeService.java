8361b5430d9f30e923a8d1e28ebf3b41e78bc2b1
kasymkhanova-nuraiym <241387@astanait.edu.kz>
2025-02-03 18:10:13 +0500

    Update EmployeeService.java

--- src/main/java/com/arnur/ems_arnur_oop/service/EmployeeService.java
+++ src/main/java/com/arnur/ems_arnur_oop/service/EmployeeService.java
@@ -1,30 +1,23 @@
-package com.arnur.ems_arnur_oop.service;
-
-import com.arnur.ems_arnur_oop.api.model.Employee;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class EmployeeService {
-    private List<Employee> employees = new ArrayList<>();
+    @Autowired
+    private EmployeeRepo employeeRepo;
 
     public Employee addEmployee(Employee employee) {
-        employees.add(employee);
-        return employee;
+        return employeeRepo.save(employee);
     }
 
     public List<Employee> getAllEmployees() {
-        return employees;
+        return employeeRepo.findAll();
     }
 
     public void deleteEmployee(Long id) {
-        employees.removeIf(employee -> employee.getId().equals(id));
+        employeeRepo.deleteById(id);
     }
+
     public Employee getEmployeeById(Long id) {
-        Optional<Employee> employeeOptional = emsRepository.findById(id);
-        return employeeOptional.orElseThrow(() -> new IllegalArgumentException("Employee with id " + id + " not found"));
+        return employeeRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Employee with id " + id + " not found"));
     }
 }
+
